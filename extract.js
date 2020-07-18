0c767b27fa5a8b626d7e44d678329d63548742f1
bpearce@angelo.edu <bpearce@angelo.edu>
2020-07-17 21:53:22 -0500

    implemented google speech-to-text

--- extract.js
+++ extract.js
@@ -3,6 +3,11 @@ const {
     Readable,
 } = require('stream');
 const child = require('child_process');
+const https = require('https');
+const fs = require('fs');
+
+// Env
+require('dotenv').config();
 
 // Express
 const app = require('express')();
@@ -14,8 +19,12 @@ const io = require('socket.io')(server);
 // Audio/video processing
 const ffmpeg = require('fluent-ffmpeg');
 
+// Google
+const speech = require('@google-cloud/speech');
+const speechClient = new speech.SpeechClient();
+
 // Streamlink
-const streamlink = child.spawn('streamlink', ['twitch.tv/artosis', 'audio_only', '-O']);
+const streamlink = child.spawn('streamlink', ['twitch.tv/destiny', 'audio_only', '-O']);
 
 let bufferedAudio = [];
 
@@ -23,11 +32,7 @@ streamlink.stdout.on('data', (data) => {
     bufferedAudio.push(data);
 });
 
-streamlink.stdin.on('end', function () {
-    process.stdout.write('REPL stream ended.');
-});
-
-streamlink.on('exit', function (code) {
+streamlink.on('exit', (code) => {
     console.log('Stream ended')
     process.exit(code);
 });
@@ -36,20 +41,43 @@ const runExtractor = setInterval(() => {
     extract();
 }, 3000);
 
-function extract() {
-    if (bufferedAudio.length >= 30) {
+async function extract() {
+    if (bufferedAudio.length >= 15) {
         const joinedAudio = Readable.from(Buffer.concat(bufferedAudio));
-        convert(joinedAudio, 'audio.mp3', (err) => {
+        convert(joinedAudio, 'audio.mp3', async (err) => {
             if (!err) {
-                translate();
+                const [rawTranscriptionData] = await getSpeechAnalysis();
+                const {
+                    transcript,
+                    confidence
+                } = rawTranscriptionData.results.reduce((prev, current) => {
+                    if (+current.alternatives[0].confidence > +prev.alternatives[0].confidence) {
+                        return current;
+                    } else {
+                        return prev;
+                    }
+                }).alternatives[0];
+
+                console.log(transcript, confidence);
                 stopExtracting();
             }
         });
     }
 }
 
-function translate() {
-    console.log('processing file here...')
+async function getSpeechAnalysis() {
+    const audioBytes = fs.readFileSync('./audio.mp3').toString('base64');
+    const request = {
+        config: {
+            encoding: "mp3",
+            sampleRateHertz: 44100,
+            languageCode: "en-US"
+        },
+        audio: {
+            content: audioBytes
+        }
+    };
+    return speechClient.recognize(request);
 }
 
 function convert(input, output, callback) {
